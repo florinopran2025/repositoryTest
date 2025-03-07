34312fc07fafcede40745a7b1a25775a21bab1de
Florin Opran <florinopran2025@gmail.com>
2025-03-07 18:37:21 +0200

    a new commit

--- ChessApp/chess v02.py
+++ ChessApp/chess v02.py
@@ -3,6 +3,7 @@
 class Table: 
     # this is the change i want to see in github
     #after that, i will revert it using git reset
+    # this is a new change
 
     def __init__(self):
         # column is ["A","B","C","D","E","F","G","H"] (left -> right)
